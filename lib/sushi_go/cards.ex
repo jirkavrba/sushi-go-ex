bf267ac67946e7fa257e66305b5373c7db9c987a
Jiří Vrba <jiri@vrba.dev>
2023-04-06 16:16:44 +0200

    Implement scoring collected cards

--- lib/sushi_go/cards.ex
+++ lib/sushi_go/cards.ex
@@ -13,10 +13,10 @@ defmodule SushiGo.Cards do
           | :pudding
           | :chopsticks
 
-  @type deck :: [card]
+  @type deck :: list(card)
 
   @spec create_game_deck(integer()) :: deck
-  def create_game_deck(puddings_excluded \\ 0) do
+  def create_game_deck(puddings_excluded \\ 0) when is_integer(puddings_excluded) do
     [
       List.duplicate(:tempura, 14),
       List.duplicate(:sashimi, 14),
@@ -29,9 +29,59 @@ defmodule SushiGo.Cards do
       List.duplicate(:squid_nigiri, 5),
       List.duplicate(:wasabi, 6),
       List.duplicate(:chopsticks, 4),
-      List.duplicate(:pudding, 10 - min(puddings_excluded, 10)),
+      List.duplicate(:pudding, 10 - min(puddings_excluded, 10))
     ]
     |> Enum.concat()
     |> Enum.shuffle()
   end
+
+  @spec count_maki(list(card())) :: integer()
+  def(count_maki(cards) when is_list(cards)) do
+    cards
+    |> Enum.map(fn :one_maki -> 1; :two_maki -> 2; :three_maki -> 3; _ -> 0 end)
+    |> Enum.sum()
+  end
+
+  @spec score(list(card()), integer()) :: integer()
+  def score(cards, maki_points \\ 0) when is_list(cards) and is_integer(maki_points) do
+    count_type = fn enum, type -> Enum.count(enum, &(&1 == type)) end
+
+    Enum.sum([
+      # Maki points are scored globally for the whole room
+      maki_points,
+
+      # Every pair of tempuras is worth 5 points
+      floor(count_type.(cards, :tempura) / 2) * 5,
+
+      # Every triplet of sashimi is worth 10 points
+      floor(count_type.(cards, :sashimi) / 3) * 10,
+
+      # Nigiri can be tripled in value by using wasabi
+      score_nigiri(cards),
+
+      # Dumplings are scored based on the number of cards owned
+      case count_type.(cards, :dumpling) do
+        0 -> 0
+        1 -> 1
+        2 -> 3
+        3 -> 6
+        4 -> 10
+        _ -> 15
+      end
+    ])
+  end
+
+  @spec score_nigiri(list(card())) :: integer()
+  defp score_nigiri(cards) when is_list(cards) do
+    cards
+    |> Enum.map(fn :wasabi -> :wasabi; :egg_nigiri -> 1; :salmon_nigiri -> 2; :squid_nigiri -> 3; _ -> nil end)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.reduce({1, 0}, fn card, {wasabi_multiplier, points} ->
+      if (card == :wasabi),
+        do: {3, points},
+        else: {1, points + card * wasabi_multiplier}
+    end)
+    |> then(fn {_, points} -> points end)
+  end
+
 end
--- test/sushi_go/cards_test.exs
+++ test/sushi_go/cards_test.exs
@@ -1,5 +1,5 @@
 defmodule SushiGo.CardsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   alias SushiGo.Cards
 
   test "deck can be created with all puddings" do
@@ -21,5 +21,45 @@ defmodule SushiGo.CardsTest do
 
     assert length(deck) == 98
     assert length(Enum.filter(deck, &(&1 == :pudding))) == 0
+  end
+
+  test "scoring includes maki points" do
+    assert Cards.score([], 6) == 6
+  end
+
+  test "scoring tempuras" do
+    assert Cards.score(List.duplicate(:tempura, 1)) == 0
+    assert Cards.score(List.duplicate(:tempura, 2)) == 5
+    assert Cards.score(List.duplicate(:tempura, 3)) == 5
+    assert Cards.score(List.duplicate(:tempura, 4)) == 10
+    assert Cards.score(List.duplicate(:tempura, 5)) == 10
+  end
+
+  test "scoring sashimis" do
+    assert Cards.score(List.duplicate(:sashimi, 1)) == 0
+    assert Cards.score(List.duplicate(:sashimi, 2)) == 0
+    assert Cards.score(List.duplicate(:sashimi, 3)) == 10
+    assert Cards.score(List.duplicate(:sashimi, 4)) == 10
+    assert Cards.score(List.duplicate(:sashimi, 5)) == 10
+    assert Cards.score(List.duplicate(:sashimi, 6)) == 20
+    assert Cards.score(List.duplicate(:sashimi, 7)) == 20
+  end
+
+  test "scoring nigiri" do
+    # 3 * 1 + 2 + 2 = 7
+    assert Cards.score([:wasabi, :egg_nigiri, :salmon_nigiri, :sashimi, :tempura, :salmon_nigiri]) == 7
+
+    # 1 + 3 * 3 + 3 * 2 + 2 = 18
+    assert Cards.score([:egg_nigiri, :tempura, :wasabi, :sashimi, :squid_nigiri, :pudding, :wasabi, :salmon_nigiri, :salmon_nigiri]) == 18
+  end
+
+  test "scoring dumplings" do
+    assert Cards.score(List.duplicate(:dumpling, 0)) == 0
+    assert Cards.score(List.duplicate(:dumpling, 1)) == 1
+    assert Cards.score(List.duplicate(:dumpling, 2)) == 3
+    assert Cards.score(List.duplicate(:dumpling, 3)) == 6
+    assert Cards.score(List.duplicate(:dumpling, 4)) == 10
+    assert Cards.score(List.duplicate(:dumpling, 5)) == 15
+    assert Cards.score(List.duplicate(:dumpling, 6)) == 15
   end
 end
