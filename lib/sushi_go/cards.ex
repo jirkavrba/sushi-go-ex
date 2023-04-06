02d51f6f21db9fa810d69a4db72391667d0b1159
Jiří Vrba <jiri@vrba.dev>
2023-04-06 14:19:24 +0200

    Add type definition for game cards

---
+++ lib/sushi_go/cards.ex
@@ -0,0 +1,15 @@
+defmodule SushiGo.Cards do
+  @type card ::
+          :egg_nigiri
+          | :salmon_nigiri
+          | :squid_nigiri
+          | :one_maki
+          | :two_maki
+          | :three_maki
+          | :tempura
+          | :sashimi
+          | :wasabi
+          | :dumpling
+          | :pudding
+          | :chopsticks
+end
